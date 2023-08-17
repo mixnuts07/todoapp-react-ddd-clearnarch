df85d7b7ca079bb49b7f5b45600d703100292874
mixnuts07 <french0719@gmail.com>
2023-08-17 22:33:41 +0900

    add domain

---
+++ domains/Todo.ts
@@ -0,0 +1,11 @@
+import Id from "./Id";
+import Title from "./Title";
+import Description from "./Description";
+
+export default class Todo {
+  constructor() {}
+
+  id: Id;
+  title: Title;
+  description: Description;
+}
