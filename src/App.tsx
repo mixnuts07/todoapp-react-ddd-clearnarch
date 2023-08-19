dd47a7d7916b0344f2feddcf90049bea3bd2a77b
mixnuts07 <french0719@gmail.com>
2023-08-19 14:14:10 +0900

    add todo

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,67 @@ import Title from "../domains/Title";
 import Description from "../domains/Description";
 
 function App() {
-  const id = new Id(1);
-  const title = new Title("TITLE");
-  const description = new Description("DESCRITPION");
-  const tmp: Todo[] = [
-    {
-      id: id,
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  // const todos = [
+  //   {
+  //     id: Date.now(),
+  //     title: title,
+  //     description: description,
+  //   },
+  // ];
+  // const id = new Id(1);
+  // const title = new Title("TITLE");
+  // const description = new Description("DESCRITPION");
+  // const tmp: Todo[] = [
+  //   {
+  //     id: id,
+  //     title: title,
+  //     description: description,
+  //   },
+  // ];
+  // const sample: Todo[] = [
+  //   {
+  //     id: new Id(2),
+  //     title: title,
+  //     description: description,
+  //   },
+  // ];
+  const addTodo = () => {
+    const newTodo = {
+      id: Date.now(),
       title: title,
       description: description,
-    },
-  ];
-  const addTodo = () => {
-    setTodo(...todo);
+    };
+    setTodos([...todos, newTodo]);
+    setTitle("");
+    setDescription("");
   };
 
-  const [todo, setTodo] = useState<Todo[]>(tmp);
+  // const [todo, setTodo] = useState<Todo[]>(tmp);
   return (
     <div className="text-amber-800">
-      <input type="text" />
-      <button onClick={() => addTodo}>ADD TODO</button>
-      {todo.map((it) => (
-        <div key={it.id.id}>
-          <h1>{it.id.id}</h1>
-          <h1>{it.title.title}</h1>
-          <h1>{it.description.description}</h1>
+      <div className="">
+        <input
+          type="text"
+          placeholder="input your title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="input your description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button onClick={() => addTodo()}>ADD TODO</button>
+      </div>
+      {todos.map((it) => (
+        <div key={it.id}>
+          <h1>{it.id}</h1>
+          <h1>{it.title}</h1>
+          <h1>{it.description}</h1>
         </div>
       ))}
     </div>
